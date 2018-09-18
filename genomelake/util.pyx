843558d10e18255a2c1c8200923bd153142d9e35
GabrielHoffman <gabrielhoffman@typhon.wireless.mountsinai.org>
2018-09-18 14:30:47 -0400

    test ambiguity codes

--- genomelake/util.pyx
+++ genomelake/util.pyx
@@ -1,6 +1,7 @@
 # cython: embedsignature=True
 import os
 import sys
+import numpy as np
 
 from libc.math cimport isnan
 
@@ -55,3 +56,37 @@ cpdef nan_to_zero(float[:] arr):
     for k in range(arr.size):
         if isnan(arr[k]):
             arr[k] = 0
+
+
+cpdef void one_hot_encode_sequence_v2(str seq, float[:, :] encoded) except *:
+    cdef size_t row_idx = 0
+
+    if encoded.shape[0] != len(seq):
+        raise ValueError('encoded array not the same length as given seq')
+
+    if encoded.shape[1] != 4:
+        raise ValueError('encoded array needs to have 4 columns')
+    
+    hsh = {
+    'A': [1., 0., 0., 0.], 
+    'C': [0., 1., 0., 0.], 
+    'G': [0., 0., 1., 0.], 
+    'T': [0., 0., 0., 1.], 
+    'N': [0.25, 0.25, 0.25, 0.25],
+    'R': [0.5, 0. , 0.5, 0. ], 
+    'Y': [0. , 0.5, 0. , 0.5], 
+    'S': [0. , 0.5, 0.5, 0. ], 
+    'W': [0.5, 0. , 0. , 0.5], 
+    'K': [0. , 0. , 0.5, 0.5], 
+    'M': [0.5, 0.5, 0. , 0. ], 
+    'B': [0.        , 0.33333333, 0.33333333, 0.33333333], 
+    'D': [0.33333333, 0.        , 0.33333333, 0.33333333], 
+    'H': [0.33333333, 0.33333333, 0.        , 0.33333333], 
+    'V': [0.33333333, 0.33333333, 0.33333333, 0.        ]}
+   
+    for base in seq:
+        value = hsh[base]
+        for i in range(0,4):
+            encoded[row_idx,i] = value[i]
+        # encoded[row_idx, ] = np.array([2.,2.,2.,2.])
+        row_idx += 1
